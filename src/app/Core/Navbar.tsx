cd5e0db2a214da30fed1f394634d360ff6953c2e
Edoardo Vincenzi <85701416+edoardovincenzi@users.noreply.github.com>
2022-09-07 08:35:40 +0200

    Update Navbar.tsx
    changing w-screen to w-full for fixing graphic bug

--- src/app/Core/Navbar.tsx
+++ src/app/Core/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="py-4 text-center bg-indigo-500 w-screen flex flex-col [&>*]:text-white">
+    <div className="py-4 text-center bg-indigo-500 w-full flex flex-col [&>*]:text-white">
       <Link to={'/'}>
         <h1 className="text-3xl">User management</h1>
       </Link>
